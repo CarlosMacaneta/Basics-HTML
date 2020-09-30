96e0fd085252508ceca477e563c664311e73fb21
CarlosMacaneta <carlosmacaneta@hotmail.com>
2020-09-30 13:28:26 +0200

    Update funcoes.js

--- GoogleGlass/_javascript/funcoes.js
+++ GoogleGlass/_javascript/funcoes.js
@@ -1,3 +1,9 @@
 function mudarFoto(foto) {
     document.getElementById("icon").src = foto;
 }
+
+function calcularTotal(){
+    var qtd = document.getElementById("qtd_").value;
+
+    document.getElementById("cash").value = parseInt(qtd) * 1500;
+}
